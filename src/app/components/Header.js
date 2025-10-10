55b1ad32d790c03092f7926019568dffd6320517
Adil <t.adhil@acodez.co.in>
2025-10-10 10:29:31 +0530

    added the menu hamburger for mobile versions

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,24 +1,45 @@
+"use client";
+import { useState } from "react";
 import Link from "next/link";
 import "../globals.scss";
 import "../../styles/_header.scss";
 import "../../styles/_common.scss";
-import Button from "./EnquireBtn"; 
+import Button from "./EnquireBtn";
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <header className="header-sec">
       <div className="container">
         <div className="head-wrapper">
-          <img src="/assets/logo-bnr.svg" alt="Sobha Banner" />
-          <nav  className="nav-links-blk">
-            <Link href="/" className="nav-link">
+          {/* Left: Logo */}
+          <div className="logo-blk">
+            <Link href="/">
+              <img src="/assets/logo-bnr.svg" alt="Sobha Banner" />
+            </Link>
+          </div>
+
+          {/* Center: Nav links */}
+          <nav className={`nav-links-blk ${isMenuOpen ? "open" : ""}`}>
+            <Link href="/" className="nav-link" onClick={() => setIsMenuOpen(false)}>
               Home
             </Link>
-            <Link href="/about" className="nav-link">
+            <Link href="/about" className="nav-link" onClick={() => setIsMenuOpen(false)}>
               Projects
             </Link>
+            <Button href="/enquire" label="Enquire Now" />
           </nav>
-          <Button href="/enquire" label="Enquire Now"/>
+
+          {/* Right: Hamburger (mobile only) */}
+          <div
+            className={`hamburger ${isMenuOpen ? "active" : ""}`}
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
         </div>
       </div>
     </header>
